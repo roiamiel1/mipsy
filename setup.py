c57604e711a6fc4a3bb9cbc522604acbe630a2d8
Nick Miller <ngmiller@iastate.edu>
2014-01-04 13:10:44 -0600

    removed test module from distribution

--- setup.py
+++ setup.py
@@ -9,13 +9,12 @@ setup(
     version='0.1.0',
     author='Nick Miller',
     author_email='ngmiller@iastate.edu',
-    packages=['mipsy', 'mipsy.test'],
-    package_data={'mipsy.test': ['mipsy/test/files/*']},
+    packages=['mipsy'],
     scripts=['bin/mipsy'],
     url='http://pypi.python.org/pypi/mipsy/',
     license='LICENSE',
     description='MIPS32 assembler.',
     install_requires=[
-        'bitstring >= 3.1.2',
+        'bitstring>=3.1.2',
     ],
 )
