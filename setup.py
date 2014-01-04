60e138618d51453c91486754240a8dcaa609edf3
Nick Miller <ngmiller@iastate.edu>
2014-01-04 13:58:21 -0600

    more meta-information fixes

--- setup.py
+++ setup.py
@@ -4,12 +4,6 @@ mipsy setup.py
 
 from distutils.core import setup
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError):
-    long_description = '(Extremely) basic MIPS32 assembler. See homepage for details.'
-
 setup(
     name='mipsy',
     version='0.1.2',
@@ -20,7 +14,7 @@ setup(
     url='https://github.com/ngmiller/mips-assembler',
     license='LICENSE',
     description='MIPS32 assembler.',
-    long_description=long_description,
+    long_description='(Extremely) basic MIPS32 assembler. See github page for details.',
     install_requires=[
         'bitstring>=3.1.2',
     ],
