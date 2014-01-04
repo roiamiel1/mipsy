4ff33187f94ddc1e6a34ee87c871ef9ef3a76320
Nick Miller <ngmiller@iastate.edu>
2014-01-04 14:10:55 -0600

    more meta fixes. Can't tell I'm new to packaging right??

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from distutils.core import setup
 
 setup(
     name='mipsy',
-    version='0.1.2',
+    version='0.1.3',
     author='Nick Miller',
     author_email='ngmiller@iastate.edu',
     packages=['mipsy'],
     scripts=['bin/mipsy'],
     url='https://github.com/ngmiller/mips-assembler',
-    license='LICENSE',
+    license='MIT',
     description='MIPS32 assembler.',
     long_description='(Extremely) basic MIPS32 assembler. See github page for details.',
     install_requires=[
